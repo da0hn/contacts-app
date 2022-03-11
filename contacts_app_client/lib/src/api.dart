df114dbc48a26efc7e71b62a503537d8d988b727
da0hn <gabrielhondacba@gmail.com>
2022-03-11 10:15:21 -0400

    feat: add `contact` model and api communication

---
+++ contacts_app_client/lib/src/api.dart
@@ -0,0 +1,68 @@
+import 'dart:_http';
+
+import 'package:dio/dio.dart';
+import 'package:equatable/equatable.dart';
+
+class Contact extends Equatable {
+  final String id;
+  final String name;
+  final String initials;
+
+  const Contact._({
+    required this.id,
+    required this.name,
+    required this.initials,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'initials': initials,
+    };
+  }
+
+  static _initial(
+    List<String> text,
+    int index,
+  ) {
+    return text[index].substring(0, 1).toUpperCase();
+  }
+
+  factory Contact.fromMap(Map<String, dynamic> map) {
+    final String name = map['name'];
+    final splitName = name.split(' ');
+    return Contact._(
+      id: map['id'] as String,
+      name: name,
+      initials: _initial(splitName, 0) + _initial(splitName, 1),
+    );
+  }
+
+  @override
+  List<String> get props => [id, name, initials];
+}
+
+class ContactRestApi {
+  final _api = Dio(BaseOptions(
+    baseUrl: 'http://localhost:8081',
+    headers: {'Content-Type': ContentType.json.mimeType},
+  ));
+
+  Future<List<Contact>> fetch() async {
+    final response = await _api.get('/contacts');
+    return (response.data['contacts'] as List)
+        .map((item) => Contact.fromMap(item))
+        .toList();
+  }
+
+  Future<Contact> add(String name) async {
+    final response = await _api.post(
+      '/contacts',
+      data: {'name': name},
+    );
+    return Contact.fromMap(response.data);
+  }
+
+  Future delete(String id) => _api.delete('/contacts/$id');
+}
